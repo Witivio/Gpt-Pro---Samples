a2e005a2ba818631b0c9c67f6a3798a9a7f1416a
Anthony Arena <aarena@witivio.com>
2024-01-04 11:03:06 +0100

    add doc

--- Samples/Asp.net Core - Support Microsoft/Asp.net Core - Support Microsoft/Model/MicrosoftSupport.cs
+++ Samples/Asp.net Core - Support Microsoft/Asp.net Core - Support Microsoft/Model/MicrosoftSupport.cs
@@ -3,27 +3,27 @@
     public class MicrosoftSupport
     {
         /// <summary>
-        /// 
+        /// Microsoft support response title
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
-        /// 
+        /// Encoded link to Microsoft Support response
         /// </summary>
         public string LinkEncoded { get; set; }
 
         /// <summary>
-        /// 
+        /// Link to Microsoft Support response
         /// </summary>
         public string Link { get; set; }
 
         /// <summary>
-        /// 
+        /// Description of the solution to be provided
         /// </summary>
         public string Description { get; set; }
 
         /// <summary>
-        /// 
+        /// Application in relation to the solution provided
         /// </summary>
         public string AppliesTo { get; set; }
     }
--- Samples/Asp.net Core - Support Microsoft/Asp.net Core - Support Microsoft/Tools/HtmlExtention.cs
+++ Samples/Asp.net Core - Support Microsoft/Asp.net Core - Support Microsoft/Tools/HtmlExtention.cs
@@ -6,10 +6,10 @@ namespace Asp.net_Core___Support_Microsoft.NewFolder
     {
         public static string RemoveHtmlFormatting(this string input)
         {
-            // Supprimer les balises HTML
+            // Remove HTML tags
             string strippedHtml = Regex.Replace(input, "<.*?>", string.Empty);
 
-            // Remplacer les caractères d'échappement HTML par leurs équivalents
+            // Replace HTML escape characters with their equivalents
             strippedHtml = System.Net.WebUtility.HtmlDecode(strippedHtml);
 
             return strippedHtml;
